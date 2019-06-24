3fd2ddab1ef72798e7d9d4094d25a402d9c467f6
KumaTea <36222458+KumaTea@users.noreply.github.com>
2019-06-25 04:23:36 +0800

    init

---
+++ AutoEval.js
@@ -0,0 +1,24 @@
+var times;  // 获取新项目频次
+var delay = 2000  // 延迟2秒，过短易出错
+var repeat;  // 总评教次数
+var item = 0;
+var goodrate = 1;  // 评优率，默认100%，请设置于[0,1]区间
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+};
+
+for (repeat=0; repeat < 5; repeat++) {
+	for (times = 0; times < 10; times++) {
+	  await sleep(delay);
+	  if (document.querySelector('[class=alls-con] .stu-eva-con').querySelectorAll('.sys-card')[item]) {document.querySelector('[class=alls-con] .stu-eva-con').querySelectorAll('.sys-card')[item].querySelector('button').click();} else {break};
+	  await sleep(delay);
+	  Array.from(document.querySelectorAll('[aria-labelledby=rcDialogTitle0] .ant-radio-group')).map(a=>a.querySelectorAll(Math.random() < goodrate ? 'input[value="0.95"]' : 'input[value="0.75"]')).map(l => l[0]).map(c => c.click());
+	  await sleep(delay);
+	  document.querySelector('.ant-modal-footer').querySelector('.ant-btn-primary').click();
+	  await sleep(delay);
+	  if (document.querySelector('.ant-modal-close') != null) {document.querySelector('.ant-modal-close').click(); item++;};  // 无法评价的情况
+	}
+	document.querySelector('[class=alls-con] .stu-eva-con').querySelector('.stu-eva-more').querySelector('i').click();
+}
+alert('评教完成。\n共' + item + '次无法评价。')
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # SYSU-Student-Evaluation
 中山大学教务系统学生自动评教脚本
+
+## 如何使用
+1. [登录教务系统](https://uems.sysu.edu.cn/jwxt)后，打开[评教页面](https://uems.sysu.edu.cn/jwxt/mk/evaluation/#/evaluation)
+2. 打开 [AutoEval.js](./AutoEval.js)，全选，复制
+3. 按F12，切换到控制台 (Console)，粘贴，回车
+4. 等待自动评教完成，剩下的均为无法评价项目
+
+## 鸣谢
+[双鸭山电报频道](https://t.me/cshs_edu_pill)的[这个脚本](https://t.me/cshs_edu_pill/273)
