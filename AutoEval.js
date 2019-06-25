e7660819dcb5ef36eab4ce4388bff44be5221bec
KumaTea <36222458+KumaTea@users.noreply.github.com>
2019-06-25 11:05:20 +0800

    async

--- AutoEval.js
+++ AutoEval.js
@@ -8,17 +8,21 @@ function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 };
 
-for (repeat=0; repeat < 5; repeat++) {
-	for (times = 0; times < 10; times++) {
-	  await sleep(delay);
-	  if (document.querySelector('[class=alls-con] .stu-eva-con').querySelectorAll('.sys-card')[item]) {document.querySelector('[class=alls-con] .stu-eva-con').querySelectorAll('.sys-card')[item].querySelector('button').click();} else {break};
-	  await sleep(delay);
-	  Array.from(document.querySelectorAll('[aria-labelledby=rcDialogTitle0] .ant-radio-group')).map(a=>a.querySelectorAll(Math.random() < goodrate ? 'input[value="0.95"]' : 'input[value="0.75"]')).map(l => l[0]).map(c => c.click());
-	  await sleep(delay);
-	  document.querySelector('.ant-modal-footer').querySelector('.ant-btn-primary').click();
-	  await sleep(delay);
-	  if (document.querySelector('.ant-modal-close') != null) {document.querySelector('.ant-modal-close').click(); item++;};  // 无法评价的情况
-	}
-	document.querySelector('[class=alls-con] .stu-eva-con').querySelector('.stu-eva-more').querySelector('i').click();
-}
-alert('评教完成。\n共' + item + '次无法评价。')
+async function autoEval() {
+	for (repeat=0; repeat < 5; repeat++) {
+		for (times = 0; times < 10; times++) {
+			await sleep(delay);
+			if (document.querySelector('[class=alls-con] .stu-eva-con').querySelectorAll('.sys-card')[item]) {document.querySelector('[class=alls-con] .stu-eva-con').querySelectorAll('.sys-card')[item].querySelector('button').click();} else {break};
+			await sleep(delay);
+			Array.from(document.querySelectorAll('[aria-labelledby=rcDialogTitle0] .ant-radio-group')).map(a=>a.querySelectorAll(Math.random() < goodrate ? 'input[value="0.95"]' : 'input[value="0.75"]')).map(l => l[0]).map(c => c.click());
+			await sleep(delay);
+			document.querySelector('.ant-modal-footer').querySelector('.ant-btn-primary').click();
+			await sleep(delay);
+			if (document.querySelector('.ant-modal-close') != null) {document.querySelector('.ant-modal-close').click(); item++;};  // 无法评价的情况
+		};
+		document.querySelector('[class=alls-con] .stu-eva-con').querySelector('.stu-eva-more').querySelector('i').click();
+	};
+	alert('评教完成。\n共' + item + '次无法评价。');
+};
+
+autoEval();
