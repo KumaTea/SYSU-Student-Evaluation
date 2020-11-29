7086235c4ddb76f2ccf54679881f20068549e159
KumaTea <36222458+KumaTea@users.noreply.github.com>
2020-11-30 01:52:39 +0800

    2020 upgrade

--- AutoEval.js
+++ AutoEval.js
@@ -1,13 +1,28 @@
-var times;  // 获取新项目频次
-var delay = 2000;  // 延迟2秒，过短易出错
-var repeat;  // 总评教次数
-var item = 0;  // Evaluation block index
-var goodrate = 1;  // 评优率，默认100%，请设置于[0,1]区间
-var finish;  // Finishing alert
+var times; // 获取新项目频次
+var delay = 2000; // 延迟2秒，过短易出错
+var repeat; // 总评教次数
+var item = 0; // Evaluation block index
+var good_rate = 1; // 评优率，默认100%，请设置于[0,1]区间
+var finish; // Finishing alert
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
-};
+}
+
+function react_input(component, value) {
+	// Credit: https://github.com/facebook/react/issues/11488#issuecomment-347775628
+	let last_value = component.value;
+	component.value = value;
+	let event = new Event('input', { bubbles: true });
+	// React 15
+	event.simulated = true;
+	// React 16
+	let tracker = component._valueTracker;
+	if (tracker) {
+	  tracker.setValue(last_value);
+	}
+	component.dispatchEvent(event);
+}
 
 async function autoEval() {
 	for (repeat=0; repeat < 5; repeat++) {
@@ -15,19 +30,24 @@ async function autoEval() {
 
 			await sleep(delay);
 
-			if (document.querySelector('[class=alls-con] .stu-eva-con').querySelectorAll('.sys-card')[item]) 
+			if (document.querySelector('.stu-eva-con').querySelectorAll('.sys-card')[item])
 			// Select an evaluation block
 
-				{document.querySelector('[class=alls-con] .stu-eva-con').querySelectorAll('.sys-card')[item].querySelector('button').click();} 
+				{document.querySelector('.stu-eva-con').querySelectorAll('.sys-card')[item].querySelector('button').click();}
 				// Click the start button
 
 			else
-				{break};
+				{break;}
 				// No available evaluation block, exit the function
 
 			await sleep(delay);
 
-			Array.from(document.querySelectorAll('[aria-labelledby=rcDialogTitle0] .ant-radio-group')).map(a=>a.querySelectorAll(Math.random() < goodrate ? 'input[value="0.95"]' : 'input[value="0.75"]')).map(l => l[0]).map(c => c.click());
+			var table_rows = document.querySelectorAll('.ant-table-wrapper')[1].querySelector('.ant-table-tbody').querySelectorAll('.ant-table-row');
+			for (var i = 0; i < table_rows.length; i++) {
+				var table_column = table_rows[i].querySelectorAll('td');
+				react_input(table_rows[i].querySelector('input.ant-input'),
+					Math.round(good_rate * parseInt(table_column[table_column.length-2].textContent)));
+			}
 			// Click all option refer to good evaluating rate
 
 			await sleep(delay);
@@ -37,25 +57,26 @@ async function autoEval() {
 
 			await sleep(delay);
 
-			var captcha = true
+			var captcha = true;
+			var block_img;
 			try{
-				var block_img = document.querySelector('.ant-modal-body').querySelectorAll('img')}
+				block_img = document.querySelector('.ant-modal-body').querySelectorAll('img');}
 			catch(e){
-				var block_img = [];
+				block_img = [];
 				captcha = false;
 			}
 			while (captcha) {
 				if (block_img.length > 0) {
-					if (block_img[block_img.length - 1].alt == "验证码") {alert("请手动输入验证码（区分大小写）！");}
+					if (block_img[block_img.length - 1].alt == "验证码") {alert("请手动输入验证码，然后稍作等待。");}
 					else {
 						captcha = false;
 						break;
-					};
-				} 
+					}
+				}
 				else {
 						captcha = false;
 						break;
-					};
+					}
 
 				await sleep(delay * 10);
 
@@ -64,30 +85,30 @@ async function autoEval() {
 				// Submit again
 				}
 				catch(e){
-					null;
-					// User have clicked
-				};
+					void(0);
+					// Do nothing: user have clicked
+				}
 				await sleep(delay);
 				try{
-					var block_img = document.querySelector('.ant-modal-body').querySelectorAll('img')}
+					block_img = document.querySelector('.ant-modal-body').querySelectorAll('img');}
 				catch(e){
-					var block_img = [];
+					block_img = [];
 					captcha = false;
-				};
-			};
+				}
+			}
 			// Detect if manually input captcha is needed
 
-			if (document.querySelector('.ant-modal-close') != null) {document.querySelector('.ant-modal-close').click(); item++;};
+			if (document.querySelector('.ant-modal-close') != null) {document.querySelector('.ant-modal-close').click(); item++;}
 			// Count if evaluation failed
-		};
+		}
 
-		document.querySelector('[class=alls-con] .stu-eva-con').querySelector('.stu-eva-more').querySelector('i').click();
+		document.querySelector('.stu-eva-con').querySelector('.stu-eva-more').querySelector('i').click();
 		// Click get more
-	};
+	}
 
-	if (item > 0) {finish = "评教完成。\n共" + item + "次无法评价。"} else {finish = "评教已顺利完成。"};
+	if (item > 0) {finish = "评教完成。\n共" + item + "次无法评价。";} else {finish = "评教已顺利完成。";}
 
 	alert(finish);
-};
+}
 
 autoEval();
--- README.md
+++ README.md
@@ -2,30 +2,63 @@
 中山大学教务系统学生自动评教脚本
 
 ## 更新
+### 2020
+
+一年过去，今年教务系统有两项升级：
+
+* JavaScript 脚本中大量加入 `debugger` 语句，阻止控制台的使用
+* 评分由按钮更改为输入框，常规状态下必须使用键盘输入
+
+因此，针对这两项改动升级了脚本，以及更改了使用步骤。
+
+### 2019
 
 * 2019秋季学期新版教务评教系统，出现了区分大小写的验证码，出现概率为首次100%及随后约20%。
 * 本次更新加入了检测验证码的机制。
   * Console出现红字POST无需惊慌
-* 鸣谢：[双鸭山电报小组](https://t.me/sbddy2019)给予我的启发和帮助
+
 ---
 
 ## 如何使用
+
+由于今年教务系统加入了阻止使用控制台的措施，以下分为两种方式。
+
+### 油猴脚本（推荐）
+
+使用[油猴](https://en.wikipedia.org/wiki/Tampermonkey)脚本可以避免开启控制台。
+
+确保你已经安装了对应[插件](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo)。
+
+1. 安装这个脚本：[**中大自动评教**](https://greasyfork.org/zh-CN/scripts/417056-%E4%B8%AD%E5%A4%A7%E8%87%AA%E5%8A%A8%E8%AF%84%E6%95%99)
+2. [登录教务系统](https://jwxt.sysu.edu.cn)后，打开[评教页面](https://jwxt.sysu.edu.cn/jwxt/mk/evaluation/#/evaluation)
+3. 等待自动评教完成
+
+### 控制台
+
 1. [登录教务系统](https://jwxt.sysu.edu.cn/)后，打开[评教页面](https://jwxt.sysu.edu.cn/jwxt/mk/evaluation/#/evaluation)
-2. 打开 [AutoEval.js](./AutoEval.js)，全选，复制
-* [方便Ctrl+A的版本](https://raw.githubusercontent.com/oudoubleyang/SYSU-Student-Evaluation/master/AutoEval.min.js)
-3. 切换到[评教页面](https://jwxt.sysu.edu.cn/jwxt/mk/evaluation/#/evaluation)，按F12打开控制台 (Console)，粘贴代码，回车
-* 如果没有控制台，请切换到Chromium内核浏览器
-4. 等待自动评教完成。若有无法评价的项目，将会提示。
+2. 打开 [AutoEval.js](./AutoEval.js)，全选复制。也可选择 [精简版本](https://raw.githubusercontent.com/KumaTea/SYSU-Student-Evaluation/master/AutoEval.min.js)
+3. 切换到[评教页面](https://jwxt.sysu.edu.cn/jwxt/mk/evaluation/#/evaluation)，按F12打开控制台 `Console`
+4. 此时画面变灰。点击「禁用所有断点」 <img src="./img/deact.png" width="193"> （点击查看大图），再点击继续 <img src="./img/ctn.png" width="93"> ，直至画面恢复正常。可以参考下方动图
+4. 切换到控制台 `Console` 选项卡，粘贴代码，回车执行
+5. 等待自动评教完成
+
+![Activate ConSoLe](./img/csl.gif)
 
 ## FAQ
-* 一定要使用Chromium内核浏览器
-* 如果运行出错，请更新浏览器
-* 好评率可以调```goodrate```参数，但是……尽量全五星吧（
-* 如果你不知道什么是Console，请点击下图查看
-<img src="https://github.com/oudoubleyang/Hosting/raw/master/SYSU-Student-Evaluation/console.png" width="300">
+
+* 推荐使用 `Chromium` 内核浏览器，如
+  * [Chrome](https://www.google.com/chrome)
+  * [**新版** Microsoft Edge](https://www.microsoft.com/en-us/edge)
+* 如果运行出错，请更新浏览器版本
+* 默认为满分。可以调节 `good_rate` 参数，评分依据该参数乘以总分，四舍五入得出
 
 ## 鸣谢
-**[双鸭山电报频道](https://t.me/cshs_edu_pill)的[这个脚本](https://t.me/cshs_edu_pill/273)**
+* [双鸭山电报频道](https://t.me/cshs_edu_pill)的[这个脚本](https://t.me/cshs_edu_pill/273)
+* [React](https://reactjs.org) 项目的[这个 issue](https://github.com/facebook/react/issues/11488) 中[**这条回复**](https://github.com/facebook/react/issues/11488#issuecomment-347775628)
 
 ## 免责声明
 * 本脚本虽然经过测试，但无法保证适用所有情况，且作者不承担因程序出错产生的任何后果。
+
+## 后记
+* 感谢所有老师的辛勤付出
+* 感谢教务系统的升级让我学到了更多 Web 编程知识
